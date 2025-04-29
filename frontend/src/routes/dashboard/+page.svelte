1eb827b6f235fb8d99a9da42f4121ba06654de7b
ferizoozoo <farhadzohoorz@gmail.com>
2025-04-29 22:36:51 +0200

    refactor: use skeletons for the initial layout of dashboard

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,21 +1,17 @@
 <div class="grid grid-cols-1 grid-rows-[1fr_4fr] gap-4 p-4 md:h-screen">
 	<!-- balance display -->
-	<div class="bg-blue-100 p-4" placeholder="balance-display">balance</div>
+	<div class="skeleton p-4" placeholder="balance-display">balance</div>
 
 	<!-- Rest of the Dashboard grid -->
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-[2fr_1fr]">
 		<!-- Market data -->
-		<div class="w-full bg-blue-100 p-4" placeholder="market-data">market-data</div>
+		<div class="skeleton w-full p-4" placeholder="market-data">market-data</div>
 
 		<!-- User-related data Grid -->
 		<div class="grid w-full grid-rows-[4fr_2fr_1fr] gap-4">
-			<div class="w-full bg-blue-100 p-4" placeholder="transaction-history">
-				transaction-history
-			</div>
-			<div class="w-full bg-blue-100 p-4" placeholder="buy-and-sell-orders">
-				buy-and-sell-orders
-			</div>
-			<div class="w-full bg-blue-100 p-4" placeholder="user">user</div>
+			<div class="skeleton w-full p-4" placeholder="transaction-history">transaction-history</div>
+			<div class="skeleton w-full p-4" placeholder="buy-and-sell-orders">buy-and-sell-orders</div>
+			<div class="skeleton w-full p-4" placeholder="user">user</div>
 		</div>
 	</div>
 </div>
